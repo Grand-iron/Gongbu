b95b409d70296a2ff4c8a3a4ced33b7173126854
Youngjo Kim <youngjo2305@gmail.com>
2022-11-13 22:39:51 +0900

    second

---
+++ 2-3.py
@@ -0,0 +1,41 @@
+base = 3
+height=4
+area = base * height /2
+print('삼각형의 면적 =', area)
+
+no=None
+print(no)
+print(type(no))
+
+b1= True
+b2=False
+b3=True
+
+print(type(b1),b1)
+print(type(b2),b2)
+
+Oct_a=0o177
+Oct_b=0O251
+hex_a=0x8ff
+hex_b=0x10
+
+print('8진수 0o177은 십진수',Oct_a)
+print('8진수 0O251은 십진수',Oct_b)
+print('16진수 0x8ff은 십진수',hex_a)
+print('16진수 0x10은 십진수',hex_b)
+
+f1=1.2
+f2=-0.45
+print(type(f1),f1)
+print(f2,type(f2))
+
+f3 =4.24E10
+f4= 4.24e-2
+print(f3)
+print(f4)
+
+e= 2-3j
+print(e.real)
+print(e.imag)
+print(type(e))
+
