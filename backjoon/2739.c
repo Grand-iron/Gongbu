0027c6e699a8bc74005abc076f83e08def8a6751
Youngjo Kim <youngjo2305@gmail.com>
2022-12-30 16:44:16 +0900

    End

---
+++ backjoon/2739.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void){
+    int a,b;
+    b=1;
+    scanf("%d",&a);
+    while (b<10){
+        printf("%d * %d = %d\n",a,b,b*a); 
+        b+=1;
+    }
+    return 0;           
+}
---
+++ backjoon/2741.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int a,n;
+    scanf("%d",&n);
+    a=1;
+    while (a<=n){
+        printf("%d\n",a);
+        a+=1;
+    }
+    return 0;
+}
